c3f24627c288c03f15850b28bb2d60fe240fe21d
Paul Grigoras <paul.grigoras90@gmail.com>
2014-10-03 22:51:56 +0100

    Initial maven project import.

---
+++ src/main/java/com/colibri/social_story/App.java
@@ -0,0 +1,13 @@
+package com.colibri.social_story;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
