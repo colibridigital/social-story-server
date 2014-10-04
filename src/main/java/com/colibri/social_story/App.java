c833fed3789e5f0f5f16b5893f52d5f2c7eff79b
Paul Grigoras <paul.grigoras90@gmail.com>
2014-10-04 01:46:32 +0100

    Add a sad bot that removes all messages and leaves.

--- src/main/java/com/colibri/social_story/App.java
+++ src/main/java/com/colibri/social_story/App.java
@@ -1,13 +1,65 @@
 package com.colibri.social_story;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import com.firebase.client.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static final String FB_URL = "https://sizzling-torch-6706.firebaseio.com/messages";
+    public static final int MESSAGE_COUNT = 3;
+
+    // Waits for a few messages, prints IDK, removes all messages and exits
+    public void run() throws InterruptedException {
+        final Firebase fb = new Firebase(FB_URL);
+        syncReadMessages(fb, MESSAGE_COUNT);
+        syncWrite(fb);
+        syncClear(fb);
+    }
+
+    private void syncReadMessages(Firebase fb, int messageCount)
+            throws InterruptedException {
+        final CountDownLatch done = new CountDownLatch(messageCount);
+        fb.addChildEventListener(new FirebaseChildEventListenerAdapter() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                done.countDown();
+                System.out.println(dataSnapshot.getValue());
+            }
+        });
+        done.await();
+    }
+
+    private void syncWrite(final Firebase fb) throws InterruptedException {
+        Map<String, String> mp = new HashMap<>();
+        mp.put("name", "paul");
+        mp.put("text", "I don't care!");
+        final CountDownLatch done = new CountDownLatch(1);
+        fb.push().setValue(mp, new Firebase.CompletionListener() {
+            @Override
+            public void onComplete(FirebaseError firebaseError, Firebase firebase) {
+                done.countDown();
+            }
+        });
+        done.await();
+    }
+
+    private void syncClear(final Firebase fb) throws InterruptedException {
+        final CountDownLatch done = new CountDownLatch(1);
+        fb.removeValue(new Firebase.CompletionListener() {
+            @Override
+            public void onComplete(FirebaseError firebaseError, Firebase firebase) {
+                done.countDown();
+            }
+        });
+        done.await();
+    }
+
+    public static void main( String[] args ) throws InterruptedException {
+        (new App()).run();
     }
 }
+
+
