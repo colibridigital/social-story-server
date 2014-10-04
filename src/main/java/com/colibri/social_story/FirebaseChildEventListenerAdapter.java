28e23a5e8bcb1b0a888e701764235e3f4672a911
Paul Grigoras <paul.grigoras90@gmail.com>
2014-10-04 01:46:07 +0100

    Add FB event adapter.

---
+++ src/main/java/com/colibri/social_story/FirebaseChildEventListenerAdapter.java
@@ -0,0 +1,36 @@
+package com.colibri.social_story;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.FirebaseError;
+
+/**
+ * Adapter for the Firebase Child Event Listener...
+ */
+public class FirebaseChildEventListenerAdapter implements ChildEventListener {
+
+    @Override
+    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+    }
+
+    @Override
+    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+    }
+
+    @Override
+    public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+    }
+
+    @Override
+    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+    }
+
+    @Override
+    public void onCancelled(FirebaseError firebaseError) {
+
+    }
+}
