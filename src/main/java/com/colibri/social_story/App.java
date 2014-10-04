3fe2366ef9066c7829c3fda72dffa186ef9ea204
jamescross91 <james_cross91@hotmail.com>
2014-10-04 17:25:57 +0100

    Listen for users.

--- src/main/java/com/colibri/social_story/App.java
+++ src/main/java/com/colibri/social_story/App.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 public class App {
 
-    public static final String FB_URL = "https://colibristory.firebaseio.com/messages";
+    public static final String FB_URL = "https://colibristory.firebaseio.com/social-story/1";
     public static final int MESSAGE_COUNT = 3;
 
     public static void main(String[] args) throws InterruptedException {
@@ -17,12 +17,10 @@ public class App {
     // Waits for a few messages, prints IDK, removes all messages and exits
     public void run() throws InterruptedException {
         final Firebase fb = new Firebase(FB_URL);
-        FirebaseUtils.syncReadMessages(fb, 0);
-        Map<String, String> mp = new HashMap<>();
-        mp.put("name", "paul");
-        mp.put("text", "I don't care!");
-        FirebaseUtils.syncWrite(fb, mp);
-        FirebaseUtils.syncClear(fb);
+
+        Story story = new Story(2, fb);
+        story.connect();
+
     }
 }
 
--- src/main/java/com/colibri/social_story/Story.java
+++ src/main/java/com/colibri/social_story/Story.java
@@ -1,26 +1,65 @@
 package com.colibri.social_story;
 
-import java.util.List;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
-/**
- * Created by paul-g on 04/10/14.
- */
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
 public class Story {
 
-    public Story() {
+    private final Firebase fb;
+    private int users = 0;
+    private int minUsers;
+    final CountDownLatch done = new CountDownLatch(1);
+
+    public Story(int minUsers, Firebase fb) {
+        this.minUsers = minUsers;
+        this.fb = fb;
+    }
+
+    // wait for min users and start
+    void connect() throws InterruptedException {
+        fb.child("users").addChildEventListener(new FirebaseChildEventListenerAdapter() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                addUser();
+            }
+        });
+        done.await();
+    }
+
+    private void start() {
+        System.out.println("Story started");
+    }
+
+    private void suggestionEnd() {
 
     }
 
-    /** Get the list of words for voting **/
-    List<String> getWords() {
-        return null;
-    }
-
-    void sendVoteRoundDone() {
+    private void voteEnd() {
 
     }
 
-    void sendSuggestRoundDone() {
-
+    private void roundEnd() {
+        // round == suggest + vote
     }
+
+    private void end() {
+        done.countDown();
+    }
+
+    private void addUser() {
+        System.out.println("User registered");
+        users++;
+        if (users > this.minUsers) {
+            start();
+        }
+    }
+
 }
+
+
+
