92a81bc3cbd0bcd585c9cc6cdbb18d5a33e8596c
Paul Grigoras <paul.grigoras90@gmail.com>
2014-10-08 19:59:42 +0100

    Add test for votes.

---
+++ src/test/java/com/colibri/social_story/entities/VotesTest.java
@@ -0,0 +1,18 @@
+package com.colibri.social_story.entities;
+
+import junit.framework.TestCase;
+
+public class VotesTest extends TestCase {
+
+    public void testPickWinner() {
+        Suggestions s = new Suggestions();
+        s.addSuggestion(new User("John"), "word1");
+        s.addSuggestion(new User("Tim"), "word2");
+        Votes v = s.getWordsForVote();
+        v.voteForWord("word1", new User("John"));
+        v.voteForWord("word2", new User("Tim"));
+        v.voteForWord("word1", new User("Bob"));
+        ScoredWord sw = v.pickWinner();
+        assertEquals(sw.getWord(), "word1");
+    }
+}
