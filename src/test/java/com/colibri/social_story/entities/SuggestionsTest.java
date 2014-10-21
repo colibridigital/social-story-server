310e93fef48e5df1278d6c12ee6a0a8c68dad09e
Paul Grigoras <paul.grigoras90@gmail.com>
2014-10-21 01:14:13 +0100

    Fix suggestions test.

--- src/test/java/com/colibri/social_story/entities/SuggestionsTest.java
+++ src/test/java/com/colibri/social_story/entities/SuggestionsTest.java
@@ -1,9 +1,17 @@
 package com.colibri.social_story.entities;
 
 import junit.framework.TestCase;
+import org.hamcrest.collection.IsIterableContainingInAnyOrder;
+import org.junit.Assert;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
+import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
 
 public class SuggestionsTest extends TestCase {
 
@@ -13,10 +21,9 @@ public class SuggestionsTest extends TestCase {
         s.addSuggestion(new User("Bob"), "word1");
         s.addSuggestion(new User("Tim"), "word2");
         Votes v = s.getWordsForVote();
-        Set<ScoredWord> exp = new HashSet<>();
-        exp.add(new ScoredWord(new User("John"), "word1", 0));
-        exp.add(new ScoredWord(new User("Tim"), "word2", 0));
-        assertEquals(exp, v.getWords());
+        List<String> exp = Arrays.asList("word1", "word2");
+        List<String> words = v.getWords().stream().map(ScoredWord::getWord).collect(toList());
+        Assert.assertThat(exp, containsInAnyOrder(words.toArray()));
     }
 
 }
